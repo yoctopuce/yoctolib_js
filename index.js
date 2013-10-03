577e96c4b9f935cba8e7d767abf2184e4384778d
Marshall Rose <mrose17@gmail.com>
2013-10-03 07:41:58 -0700

    get rid of old code

--- index.js
+++ index.js
@@ -6,7 +6,6 @@
 
        if (yapi.yRegisterHub('http://127.0.0.1:4444/') != yapi.YAPI_SUCCESS) ...
 
-
    note: this could load the library asynchronously, but there's only 30 files to load...
 
  */
@@ -33,37 +32,3 @@ if (i !== -1) files.splice(i, 1);
 includeSync('./Sources/yocto_api.js');
 for (i = 0; i < files.length; i++) includeSync('./Sources/' + files[i]);
 module.exports = api;
-
-if (true) return;
-
-
-
-var count;
-
-var include = function(filename, callback) {
-  fs.readFile(filename, function(err, data) {
-    if (err) throw err;
-
-    vm.runInNewContext(data.toString(), api, filename);
-    if (!!callback) callback();
-    if (--count <= 0) module.exports = api;
-  });
-};
-
-
-fs.readdir('./Sources', function(err, files) {
-  var i;
-
-  if (err) throw err;
-
-  count = files.length;
-
-  include('./Sources/yocto_api.js', function() {
-    i = files.indexOf('yocto_api.js');
-    if (i !== -1) files.splice(i, 1);
-    for (i = 0; i < files.length; i++) include('./Sources/' + files[i]);
-  });
-});
-
-
-module.exports = {};
