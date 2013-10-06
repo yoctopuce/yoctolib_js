24c594ccf8ff58fce87cd8b44b6338c7b29697c9
Marshall Rose <mrose17@gmail.com>
2013-10-06 00:26:57 -0700

    fixes, redux

--- index.js
+++ index.js
@@ -20,20 +20,15 @@ var api = { window         : { XMLHttpRequest : xhr.XMLHttpRequest }
           , XMLHttpRequest : xhr.XMLHttpRequest
           , console        : console
           , navigator      : {}
-          };
+          }
+  , includeSync = function(filename) {
+      vm.runInNewContext(fs.readFileSync(filename).toString(), api, filename);
+    }
+  , dir = __dirname + '/Sources'
+  ,  files  = fs.readdirSync(dir)
+  , i = files.indexOf('yocto_api.js')
+  ;
 
-var includeSync = function(filename) {
-  vm.runInNewContext(fs.readFileSync(filename).toString(), api, filename);
-};
-
-var files, dir;
-try {
-  files  = fs.readdirSync(dir = './Sources');
-} catch(ex) {
-  files  = fs.readdirSync(dir = './node_modules/yoctolib/Sources');
-}
-
-var i = files.indexOf('yocto_api.js');
 if (i !== -1) files.splice(i, 1);
 includeSync(dir + '/yocto_api.js');
 for (i = 0; i < files.length; i++) includeSync(dir + '/' + files[i]);
